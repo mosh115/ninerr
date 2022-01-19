8063d7d6870b861cde2d75dc0f399113849a15c8
DoronBarshap <doron.prog@gmail.com>
2022-01-19 13:35:44 +0200

    doron changed the gigapp title

--- frontend/src/pages/gig-app.jsx
+++ frontend/src/pages/gig-app.jsx
@@ -1,45 +1,35 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from "react"
+import { connect } from "react-redux"
 
-import { GigList } from '../cmps/gig-list.jsx'
+import { GigList } from "../cmps/gig-list.jsx"
 
-
-import { loadGigs, onAddGig } from '../store/gig.actions.js'
-import { showSuccessMsg } from '../services/event-bus.service.js'
+import { loadGigs, onAddGig } from "../store/gig.actions.js"
+import { showSuccessMsg } from "../services/event-bus.service.js"
 
 function _GigApp({ loadGigs, gigs, onAddGig }) {
+  useEffect(() => {
+    loadGigs()
+  }, [])
 
-    useEffect(() => {
-        loadGigs()
-    }, [])
-
-
-
-
-    return (
-        <div>
-            <h3>Explore</h3>
-            <button onClick={onAddGig}>Add Gig -will be deleted</button>
-            Todo:
-            {/* <Filter/> */}
-            <GigList gigs={gigs} className="gig-list" />
-
-
-        </div>
-    )
+  return (
+    <div>
+      <h3>Explore all</h3>
+      <button onClick={onAddGig}>Add Gig -will be deleted</button>
+      Todo:
+      {/* <Filter/> */}
+      <GigList gigs={gigs} className="gig-list" />
+    </div>
+  )
 }
-
 
 function mapStateToProps(state) {
-    return {
-        gigs: state.gigModule.gigs
-    }
+  return {
+    gigs: state.gigModule.gigs,
+  }
 }
 const mapDispatchToProps = {
-    loadGigs,
-    onAddGig,
-
+  loadGigs,
+  onAddGig,
 }
-
 
 export const GigApp = connect(mapStateToProps, mapDispatchToProps)(_GigApp)
