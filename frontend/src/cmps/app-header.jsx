71aeb74478554490a15d56168b9c3677fb6ff9de
DoronBarshap <doron.prog@gmail.com>
2022-01-19 14:22:26 +0200

    doron changed the favicon and tried to add logo to app-header

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react"
+import { connect } from "react-redux"
+import { Link, NavLink } from "react-router-dom"
+import routes from "../routes"
 
-import routes from '../routes'
-
-
-import { onLogin, onLogout, onSignup, loadUsers, removeUser } from '../store/user.actions.js'
-import { LoginSignup } from './login-signup.jsx'
+import {
+  onLogin,
+  onLogout,
+  onSignup,
+  loadUsers,
+  removeUser,
+} from "../store/user.actions.js"
+import { LoginSignup } from "./login-signup.jsx"
 
 function _AppHeader({ onLogin, onSignup, onLogout, user }) {
-
-    return (
-        <header className="app-header">
-            <div className="nav-container flex align-center">
-                <NavLink className="logo clean-link" exact to="/">Ninerr Logo</NavLink>
-                <nav>
-                    <NavLink className="clean-link" to="/about">About</NavLink>
-                    <NavLink className="clean-link" to="/explore">Explore</NavLink>
-                    <NavLink className="clean-link" to="/sign-up-seller">Become A Seller</NavLink>
-                    <NavLink className="clean-link" to="/sign-in">Sign in</NavLink>
-                    <NavLink className="clean-link" to="/sign-up-buyer">Join</NavLink>
-                    {user && <div className="user-section">{user.username}🌟</div>}
-                </nav>
-            </div>
-            {/* <nav> */}
-            {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
+  return (
+    <header className="app-header">
+      <div className="nav-container flex align-center">
+        <NavLink className="logo clean-link" exact to="/">
+          <img src="../assets/img/logo/logo.png" alt="Ninerr logo" />
+        </NavLink>
+        <nav>
+          <NavLink className="clean-link" to="/about">
+            About
+          </NavLink>
+          <NavLink className="clean-link" to="/explore">
+            Explore
+          </NavLink>
+          <NavLink className="clean-link" to="/sign-up-seller">
+            Become A Seller
+          </NavLink>
+          <NavLink className="clean-link" to="/sign-in">
+            Sign in
+          </NavLink>
+          <NavLink className="clean-link" to="/sign-up-buyer">
+            Join
+          </NavLink>
+          {user && <div className="user-section">{user.username}🌟</div>}
+        </nav>
+      </div>
+      {/* <nav> */}
+      {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
 
                 {user &&
                     <span className="user-info">
@@ -42,29 +57,30 @@ function _AppHeader({ onLogin, onSignup, onLogout, user }) {
                     </section>
                 } */}
 
-            {/* </nav> */}
+      {/* </nav> */}
 
-            {/* <h1>My App</h1> */}
-        </header>
-    )
+      {/* <h1>My App</h1> */}
+    </header>
+  )
 }
 
 function mapStateToProps(state) {
-    return {
-        users: state.userModule.users,
-        user: state.userModule.user,
-        count: state.userModule.count,
-        isLoading: state.systemModule.isLoading
-    }
+  return {
+    users: state.userModule.users,
+    user: state.userModule.user,
+    count: state.userModule.count,
+    isLoading: state.systemModule.isLoading,
+  }
 }
 const mapDispatchToProps = {
-    onLogin,
-    onSignup,
-    onLogout,
-    loadUsers,
-    removeUser
+  onLogin,
+  onSignup,
+  onLogout,
+  loadUsers,
+  removeUser,
 }
 
-
-
-export const AppHeader = connect(mapStateToProps, mapDispatchToProps)(_AppHeader)
+export const AppHeader = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_AppHeader)
